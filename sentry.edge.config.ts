a69f283d3b153b1714c4ce20b9955b1a47d4fb2a
andre25va <info@andrevargasteam.com>
2026-07-01 22:13:35 -0500

    Add Sentry error monitoring

---
+++ sentry.edge.config.ts
@@ -0,0 +1,7 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e56791c031f7f4f4a7c69f9835571486@o4511124980170752.ingest.us.sentry.io/4511663501344769",
+  tracesSampleRate: 1.0,
+  debug: false,
+});
